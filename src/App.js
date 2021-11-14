7a1fcfeb10a41d92c92fffe151052f480cb3040b
Mark de Courcy Ling <54800588+markdecl@users.noreply.github.com>
2021-11-14 22:01:33 +0000

    Add stylesheet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
+
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <FlashcardList flashcards={flashcards} />
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2 + 2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+  id: 1,
+    question: 'What is 2 + 2?',
+      answer: '4',
+        options: [
+          '2',
+          '3',
+          '4',
+          '5'
+        ]
+  },
+  {
+  id: 1,
+    question: 'What is 2 + 2?',
+      answer: '4',
+        options: [
+          '2',
+          '3',
+          '4',
+          '5'
+        ]
+  }
+]
+
 
 export default App;
---
+++ src/README.md
@@ -0,0 +1,7 @@
+# Plan
+
+## User log-in page
+
+## Main menu / Dashboard (showing user's progress)
+
+## Flashcard deck review page
